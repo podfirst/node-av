dce9c1db9177aebef64721887feb18161a890685
seydx <dev@seydx.com>
2025-10-27 10:01:12 +0100

    Add missing network header inclusion for cross-platform compatibility

--- src/bindings/format_context.cc
+++ src/bindings/format_context.cc
@@ -12,8 +12,18 @@
 
 extern "C" {
 #include <libavformat/internal.h>
-#include <libavformat/network.h>
 }
+
+#ifdef _WIN32
+  #ifndef WIN32_LEAN_AND_MEAN
+    #define WIN32_LEAN_AND_MEAN
+  #endif
+  #include <winsock2.h>
+  #include <ws2tcpip.h>
+#else
+  #include <sys/socket.h>
+  #include <netinet/in.h>
+#endif
 
 namespace ffmpeg {
 
